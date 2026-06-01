3aa6b5ef7e1147931dd05864207461d266fbae9b
Zurdo2110 <juan.irusta98@gmail.com>
2026-06-01 17:02:50 -0300

    Script de inicio y permiso para ejecucion de linux

--- PRACTICO_2_DOCUMENTACION/EjecutarLinux.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "=== 1. Compilando modificaciones ==="
-mvn clean package
-
-echo "=== 2. Iniciando la aplicación (Base de datos intacta) ==="
-java -jar target/proye-is-1.0-SNAPSHOT.jar
